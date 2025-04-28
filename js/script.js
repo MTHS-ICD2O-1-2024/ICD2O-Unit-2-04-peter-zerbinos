a70157129dd0564c3311d2e113480308289d6db2
Peter Zerbinos <peter.zerbinos@mths.ca>
2025-04-28 02:09:23 +0000

    added code to 2-04

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Peter Zerbinos All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Peter Zerbinos
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates the area of a triangle.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function onButtonClicked () {
+  // input
+  const baseOfTriangle = parseFloat(document.getElementById('base-of-triangle').value)
+  const heightOfTriangle = parseFloat(document.getElementById('height-of-triangle').value)
+
+  // process
+  const areaOfTriangle = (baseOfTriangle * heightOfTriangle)/2
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + areaOfTriangle + ' cm²'
 }
